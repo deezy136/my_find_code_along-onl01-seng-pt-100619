4efbb68c4590ace0298c3f283da5691c246dad55
deezy13 <deezy13@gmail.com>
2019-10-16 04:19:11 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,13 +1,7 @@
-require 'pry'
-
 def my_find(collection)
-
-new_collection = []
-i =0 
-while i < 100
-  
-  yield(1)
-  i+=1 
-end 
-
+  i = 0
+  while i < collection.length
+    return(collection[i]) if yield(collection[i])
+    i += 1
+  end
 end
